258c8ce2470d78614cff74f1a6b90fbecd210c12
Simon Newman <simon.newman@outlook.com>
2025-05-06 09:52:46 +0100

    fix: add WebAssembly support for tiktoken

--- next.config.js
+++ next.config.js
@@ -4,6 +4,21 @@ const nextConfig = {
     DALL_E_3_ENDPOINT: process.env.DALL_E_3_ENDPOINT,
     DALL_E_3_API_KEY: process.env.DALL_E_3_API_KEY,
   },
+  webpack: (config, { isServer }) => {
+    // Enable WebAssembly
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+    };
+
+    // Handle .wasm files
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: 'webassembly/async',
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig; 
