0ff42bc2da1ec6b4b0732d22fa3eb1ee029286b6
Simon Newman <simon.newman@outlook.com>
2025-11-17 16:14:29 +0000

    Trying to fix headers

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -2,22 +2,6 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import AzureADProvider from "next-auth/providers/azure-ad";
 import { cookies } from 'next/headers'
 
-declare module "next-auth" {
-    interface Session {
-        accessToken?: string;
-        idToken?: string;
-    }
-}
-
-declare module "next-auth/jwt" {
-    interface JWT {
-        provider?: string;
-        accessToken?: string;
-        idToken?: string;
-    }
-}
-
-
 export const authOptions: NextAuthOptions = {
     providers: [
         AzureADProvider({
@@ -28,21 +12,6 @@ export const authOptions: NextAuthOptions = {
         }),
     ],
     secret: process.env.NEXTAUTH_SECRET as string,
-    callbacks: {
-        async jwt({ token, account}) {
-            if (account) {
-                token.idToken = account.id_token as string;
-                token.accessToken = account.access_token as string;
-            }
-            return token;
-        },
-        async session({ session, token }) {
-            session.accessToken = token.accessToken as string;
-            session.idToken = token.idToken as string;
-            // (await cookies()).set('idToken', session.idToken);
-            return session;
-        },
-    },
     pages: {
         signIn: "/login",
     }
